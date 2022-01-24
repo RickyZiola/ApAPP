2b35731459a624b774a7b6317e184c8c37a5a6c6
RickyZiola <78122421+RickyZiola@users.noreply.github.com>
2022-01-24 13:51:46 -0500

    Update ApAPP.java

--- ApAPP.java
+++ ApAPP.java
@@ -2,7 +2,12 @@ import java.awt.*;
 import java.awt.*;
 
 class ApAPP {
+  ApAPP() {
+    JFrame f = new JFrame("ApAPP");
+  }
   public static void main (String[] args) {
-    
+    swingUtilities.invokeLater(new Runnable() {
+      ApAPP app = new ApAPP();
+    });
   }
 }
