cc8b3b4b87c02955267c0148341f244ec19a18a5
RickyZiola <78122421+RickyZiola@users.noreply.github.com>
2022-01-24 15:28:18 -0500

    Create ApAPP.java

--- ApAPP.java
+++ ApAPP.java
@@ -6,6 +6,8 @@ import javax.swing.*;
 public class ApAPP {
   ApAPP() {
     JFrame f = new JFrame("ApAPP");
+    JLabel greeting = new JLabel("Hello world!");
+    f.getContentPane().add(greeting);
     f.pack();
     f.setVisible(true);
     f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
